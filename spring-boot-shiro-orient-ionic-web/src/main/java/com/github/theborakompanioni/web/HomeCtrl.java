35bbfbbfb597d514e3042f7e9a36080bc0d9004b
theborakompanioni <theborakompanioni+github@gmail.com>
2015-09-18 23:40:06 +0200

    remove mappings from HomeCtrl

--- spring-boot-shiro-orient-ionic-web/src/main/java/com/github/theborakompanioni/web/HomeCtrl.java
+++ spring-boot-shiro-orient-ionic-web/src/main/java/com/github/theborakompanioni/web/HomeCtrl.java
@@ -1,20 +1,11 @@
 package com.github.theborakompanioni.web;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by void on 21.08.15.
  */
 @Controller
 public class HomeCtrl {
-    @RequestMapping("/")
-    String index() {
-        return "index";
-    }
 
-    @RequestMapping("/login")
-    String login() {
-        return "login";
-    }
 }
